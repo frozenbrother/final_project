e9a07eb7022201b832ca86fae9f74378bb8c6028
Ray Chau <raymond@wegotthis.com.au>
2022-04-26 19:43:27 +1000

    heroku please

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = pickle.load(open('model3.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/predict',methods=['POST'])
 def predict():
